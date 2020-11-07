56ae6a3036dd8ddb8a4cd852812c5aa8b602cdbb
blbranch <blbranch@gmail.com>
2020-11-07 09:51:08 -0500

    broken functional component

--- doctor-search/src/components/API/Api.js
+++ doctor-search/src/components/API/Api.js
@@ -1,51 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {
     Card, CardText, CardBody, 
     CardTitle, CardSubtitle
 } from 'reactstrap';
 const axios = require('axios');
 
-export default class ProviderApi extends React.Component {
-    state = {
-      providers: []
-    }
-  
-    componentDidMount() {
-      
+const ProviderApi = (props) => {
+const [providers, setProviders] = useState([])
+
+    useEffect(() => {
+
       axios.get('http://localhost:5000/?', {
         params: {
             //version: 2.1,
-            city: this.props.cityName,
-            state: this.props.stateName,
-            postal_code: this.props.zipcode
+            city: props.cityName,
+            state: props.stateName,
+            postal_code: props.zipcode
          }
       })
         .then(res => {
           const results = res.data.results
-          this.setState({ 
+          
+          setProviders({ 
             providers: results 
-          });
-          console.log(results)
+          })
         })
-    }
+    
+    
+      });
 
-  
-    render() {
-      const {
-         providers
-      } = this.state;
-      return (
-          <div>
-              {providers.map(provider => (
-                <Card>
+    const providerData = providers.map(provider => 
+              <Card>
                     <CardBody>
                         <CardTitle>{provider.basic.first_name} {provider.basic.last_name}</CardTitle>
                         <CardSubtitle>{provider.taxonomies[0].desc}</CardSubtitle>
                         <CardText>{provider.addresses[0].address_1}</CardText>   
                     </CardBody>
-                </Card>    
-            ))}
-          </div>
+              </Card>    
+    )
+
+    
+      return (
+        <div>
+          {providerData}
+        </div>
       )
-    }
   }
+
+export default ProviderApi
